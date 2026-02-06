54c21373ea11d74e5ec6fcd1487e7487112a7c75
Neorizuki <lcristianmarkangelo@gmail.com>
2026-02-06 11:48:33 +0800

    fix: remove grab_pay and qrph from Paymongo payment methods (not enabled on test account)

--- app/Services/PaymongoService.php
+++ app/Services/PaymongoService.php
@@ -67,7 +67,7 @@ class PaymongoService
                                     'description' => "Booking Reference: {$paymentSlip->slip_number}"
                                 ]
                             ],
-                            'payment_method_types' => ['gcash', 'paymaya', 'card', 'grab_pay', 'qrph'],
+                            'payment_method_types' => ['gcash', 'paymaya', 'card'],
                             'description' => $description,
                             'metadata' => [
                                 'payment_slip_id' => $paymentSlip->id,
